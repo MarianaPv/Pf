b7fc0cf422c2b021cc1109d3b926627d773a214d
Mariana <marianapertuz@uninorte.edu.co>
2020-06-11 00:28:39 -0500

    Updates

---
+++ precache-manifest.08833274c349fd2441ee6a9b0531dc44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2955307f909cc7aff9fb97c7c78027da",
+    "url": "/Pf/index.html"
+  },
+  {
+    "revision": "9bf7ef2350e22da9ed39",
+    "url": "/Pf/static/css/main.caeba3dd.chunk.css"
+  },
+  {
+    "revision": "3308c98cac79d39ccdb1",
+    "url": "/Pf/static/js/2.29117086.chunk.js"
+  },
+  {
+    "revision": "f5da13dbd5ce8fab13a280255e8b5700",
+    "url": "/Pf/static/js/2.29117086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf7ef2350e22da9ed39",
+    "url": "/Pf/static/js/main.d8b249b9.chunk.js"
+  },
+  {
+    "revision": "71382de38a8e18fafb6f",
+    "url": "/Pf/static/js/runtime-main.3d67f241.js"
+  },
+  {
+    "revision": "1871862d87bb8037d953317fb4497189",
+    "url": "/Pf/static/media/profile.1871862d.png"
+  },
+  {
+    "revision": "c34728ce0be1bc476cebc71a32f4d0db",
+    "url": "/Pf/static/media/warehouse4.c34728ce.png"
+  }
+]);
--- src/Routes/Routes.js
+++
@@ -1,6 +0,0 @@
-export const HOMEPAGE = "/";
-export const HOME = "/home";
-export const REGISTRO = "/register";
-export const LOGIN = "/login";
-export const AGREGAR = "/agregar-productos";
-export const PERFIL = "/mi-perfil";
